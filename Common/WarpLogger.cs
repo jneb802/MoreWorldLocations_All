580a71c9bcd8b2ac6c2b0a767c2a441afef86a03
jneb802 <73610029+jneb802@users.noreply.github.com>
2024-04-09 14:09:28 -0700

    Add back WarpLogger to common

---
+++ Common/WarpLogger.cs
@@ -0,0 +1,9 @@
+using BepInEx.Logging;
+
+namespace Common;
+
+public class WarpLogger
+{
+    public static readonly ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource("MoreWorldLocations");
+
+}
