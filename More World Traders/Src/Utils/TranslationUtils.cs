b0f19111f8d733536e7c91ef0b4301e2d6af039a
jneb802 <73610029+jneb802@users.noreply.github.com>
2024-12-30 12:41:06 -0800

    Add translations

--- More World Traders/Src/Utils/TranslationUtils.cs
+++ More World Traders/Src/Utils/TranslationUtils.cs
@@ -14,7 +14,9 @@ public class TranslationUtils
         Localization.AddTranslation("English", new Dictionary<string, string>
         {
             {"$se_blacksmithStone", "Blacksmith Stone"},
-            {"$item_mwl_blacksmithstone", "Blacksmith Stone"},
+            {"$item_mwl_blacksmithstone_tier1", "Blacksmith Stone (1)"},
+            {"$item_mwl_blacksmithstone_tier2", "Blacksmith Stone (2)"},
+            {"$item_mwl_blacksmithstone_tier3", "Blacksmith Stone (3)"},
             {"$item_mwl_blacksmithstone_description", "Upgrades an armor or weapon past max quality. Place an armor or weapon in top left cell in inventory and consume this item."},
         });
     }
