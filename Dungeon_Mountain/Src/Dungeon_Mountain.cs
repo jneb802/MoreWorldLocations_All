76577a8d0073d1dc7556645a235e0d992554d7e9
jneb802 <73610029+jneb802@users.noreply.github.com>
2025-01-02 13:26:57 -0800

    initial commit mountain dungeon

---
+++ Dungeon_Mountain/Src/Dungeon_Mountain.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using JetBrains.Annotations;
+using LocalizationManager;
+using ServerSync;
+using UnityEngine;
+
+namespace Dungeon_Mountain
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class Dungeon_MountainPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "Dungeon_Mountain";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "warpalicious";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource Dungeon_MountainLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        // Location Manager variables
+        public Texture2D tex = null!;
+
+        // Use only if you need them
+        //private Sprite mySprite = null!;
+        //private SpriteRenderer sr = null!;
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+            bool saveOnSet = Config.SaveOnConfigSet;
+            Config.SaveOnConfigSet =
+                false;
+
+            _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On,
+                "If on, the configuration is locked and can be changed by server admins only.");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+
+            if (saveOnSet)
+            {
+                Config.SaveOnConfigSet = saveOnSet;
+                Config.Save();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                Dungeon_MountainLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                Dungeon_MountainLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                Dungeon_MountainLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            [UsedImplicitly] public int? Order = null!;
+            [UsedImplicitly] public bool? Browsable = null!;
+            [UsedImplicitly] public string? Category = null!;
+            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer = null!;
+        }
+
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", UnityInput.Current.SupportedKeyCodes);
+        }
+
+        #endregion
+    }
+
+    public static class KeyboardExtensions
+    {
+        public static bool IsKeyDown(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKeyDown(shortcut.MainKey) &&
+                   shortcut.Modifiers.All(Input.GetKey);
+        }
+
+        public static bool IsKeyHeld(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKey(shortcut.MainKey) &&
+                   shortcut.Modifiers.All(Input.GetKey);
+        }
+    }
+}
---
+++ Dungeon_Mountain/VersionHandshake.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using HarmonyLib;
+
+namespace Dungeon_Mountain
+{
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.OnNewConnection))]
+    public static class RegisterAndCheckVersion
+    {
+        private static void Prefix(ZNetPeer peer, ref ZNet __instance)
+        {
+            // Register version check call
+            Dungeon_MountainPlugin.Dungeon_MountainLogger.LogDebug("Registering version RPC handler");
+            peer.m_rpc.Register($"{Dungeon_MountainPlugin.ModName}_VersionCheck",
+                new Action<ZRpc, ZPackage>(RpcHandlers.RPC_Dungeon_Mountain_Version));
+
+            // Make calls to check versions
+            Dungeon_MountainPlugin.Dungeon_MountainLogger.LogDebug("Invoking version check");
+            ZPackage zpackage = new();
+            zpackage.Write(Dungeon_MountainPlugin.ModVersion);
+            peer.m_rpc.Invoke($"{Dungeon_MountainPlugin.ModName}_VersionCheck", zpackage);
+        }
+    }
+
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.RPC_PeerInfo))]
+    public static class VerifyClient
+    {
+        private static bool Prefix(ZRpc rpc, ZPackage pkg, ref ZNet __instance)
+        {
+            if (!__instance.IsServer() || RpcHandlers.ValidatedPeers.Contains(rpc)) return true;
+            // Disconnect peer if they didn't send mod version at all
+            Dungeon_MountainPlugin.Dungeon_MountainLogger.LogWarning(
+                $"Peer ({rpc.m_socket.GetHostName()}) never sent version or couldn't due to previous disconnect, disconnecting");
+            rpc.Invoke("Error", 3);
+            return false; // Prevent calling underlying method
+        }
+
+        private static void Postfix(ZNet __instance)
+        {
+            ZRoutedRpc.instance.InvokeRoutedRPC(ZRoutedRpc.instance.GetServerPeerID(),
+                $"{Dungeon_MountainPlugin.ModName}RequestAdminSync",
+                new ZPackage());
+        }
+    }
+
+    [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.ShowConnectError))]
+    public class ShowConnectionError
+    {
+        private static void Postfix(FejdStartup __instance)
+        {
+            if (__instance.m_connectionFailedPanel.activeSelf)
+            {
+                __instance.m_connectionFailedError.fontSizeMax = 25;
+                __instance.m_connectionFailedError.fontSizeMin = 15;
+                __instance.m_connectionFailedError.text += "\n" + Dungeon_MountainPlugin.ConnectionError;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.Disconnect))]
+    public static class RemoveDisconnectedPeerFromVerified
+    {
+        private static void Prefix(ZNetPeer peer, ref ZNet __instance)
+        {
+            if (!__instance.IsServer()) return;
+            // Remove peer from validated list
+            Dungeon_MountainPlugin.Dungeon_MountainLogger.LogInfo(
+                $"Peer ({peer.m_rpc.m_socket.GetHostName()}) disconnected, removing from validated list");
+            _ = RpcHandlers.ValidatedPeers.Remove(peer.m_rpc);
+        }
+    }
+
+    public static class RpcHandlers
+    {
+        public static readonly List<ZRpc> ValidatedPeers = new();
+
+        public static void RPC_Dungeon_Mountain_Version(ZRpc rpc, ZPackage pkg)
+        {
+            string? version = pkg.ReadString();
+
+            Dungeon_MountainPlugin.Dungeon_MountainLogger.LogInfo("Version check, local: " +
+                                                                  Dungeon_MountainPlugin.ModVersion +
+                                                                  ",  remote: " + version);
+            if (version != Dungeon_MountainPlugin.ModVersion)
+            {
+                Dungeon_MountainPlugin.ConnectionError =
+                    $"{Dungeon_MountainPlugin.ModName} Installed: {Dungeon_MountainPlugin.ModVersion}\n Needed: {version}";
+                if (!ZNet.instance.IsServer()) return;
+                // Different versions - force disconnect client from server
+                Dungeon_MountainPlugin.Dungeon_MountainLogger.LogWarning(
+                    $"Peer ({rpc.m_socket.GetHostName()}) has incompatible version, disconnecting...");
+                rpc.Invoke("Error", 3);
+            }
+            else
+            {
+                if (!ZNet.instance.IsServer())
+                {
+                    // Enable mod on client if versions match
+                    Dungeon_MountainPlugin.Dungeon_MountainLogger.LogInfo(
+                        "Received same version from server!");
+                }
+                else
+                {
+                    // Add client to validated list
+                    Dungeon_MountainPlugin.Dungeon_MountainLogger.LogInfo(
+                        $"Adding peer ({rpc.m_socket.GetHostName()}) to validated list");
+                    ValidatedPeers.Add(rpc);
+                }
+            }
+        }
+    }
+}
