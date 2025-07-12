69dec1c38e1e20ec39e47606ed423f3a5da0eb96
warp <73610029+jneb802@users.noreply.github.com>
2025-07-11 18:45:35 -0700

    Add skill book localizations

--- More World Traders/Src/Utils/TranslationUtils.cs
+++ More World Traders/Src/Utils/TranslationUtils.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Jotunn.Entities;
 
@@ -14,11 +15,34 @@ public class TranslationUtils
         Localization.AddTranslation("English", new Dictionary<string, string>
         {
             {"$se_blacksmithStone", "Blacksmith Stone"},
+            {"$se_skillBook", "Skill Book"},
             {"$item_mwl_blacksmithstone_tier1", "Blacksmith Stone (1)"},
             {"$item_mwl_blacksmithstone_tier2", "Blacksmith Stone (2)"},
             {"$item_mwl_blacksmithstone_tier3", "Blacksmith Stone (3)"},
             {"$item_mwl_blacksmithstone_description", "Upgrades an armor or weapon past max quality. Place an armor or weapon in top left cell in inventory and consume this item."},
         });
+    }
+
+    public static void AddSkillBookLocalization(Skills.SkillType skill, int tier)
+    {
+        Localization = Jotunn.Managers.LocalizationManager.Instance.GetLocalization();
+    
+        Localization.AddTranslation("English", new Dictionary<string, string>
+        {
+            {"$item_mwl_skillBook_" + skill.ToString() + "_bookTier" + tier, skill.ToString() + " Skill Book " + "(" + tier + ")"},
+            {"$item_mwl_skillBook_description_" + skill.ToString() + "_bookTier" + tier, "A skill book to raise " + skill + " skill"},
+        });
+    }
+    
+    public static void BuildSkillBookLocalizations()
+    {
+        foreach (Skills.SkillType skill in Enum.GetValues(typeof(Skills.SkillType)))
+        {
+            for (int tier = 1; tier <= 3; tier++)
+            {
+                AddSkillBookLocalization(skill, tier);
+            }
+        }
     }
 
 }
