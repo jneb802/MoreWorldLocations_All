8e930f10b6f666b62ed5289ce50328c8bdddc7e5
warp <73610029+jneb802@users.noreply.github.com>
2025-07-23 12:23:58 -0700

    add biome to shrine config

--- More World Locations_AIO/Src/Shrines/ShrineConfig.cs
+++ More World Locations_AIO/Src/Shrines/ShrineConfig.cs
@@ -1,4 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
+using Jotunn.Configs;
+using Jotunn.Entities;
+using Jotunn.Managers;
+using UnityEngine;
 
 namespace More_World_Locations_AIO.Shrines;
 
@@ -8,6 +13,21 @@ public class ShrineConfig
     public string displayName;
     public StatusEffect statusEffect;
     public Shrine.ShrineOffering shrineOffering;
+    public Heightmap.Biome biome;
+
+    // A weight list of effect counts.
+    // The key is the number of effects a status effect has.
+    // The value is the weight of selecting that amount of effects
+    public Dictionary<int, int> weightedEffectCounts = new Dictionary<int, int>
+    {
+        { 1, 50 },  // 1 effect with weight 50
+        { 2, 30 },  // 2 effects with weight 30
+        { 3, 20 }   // 3 effects with weight 20
+    };
+    
+    public int duration;
+
+    public List<int> durationValues = new List<int>() { 300, 600, 1200, 1800 };
     
     public ShrineConfig(string internalName, string displayName, StatusEffect statusEffect, Shrine.ShrineOffering shrineOffering)
     {
@@ -15,6 +35,7 @@ public class ShrineConfig
         this.displayName = displayName;
         this.statusEffect = statusEffect;
         this.shrineOffering = shrineOffering;
+        this.biome = biome;
     }
     
     public Shrine.ShrineOffering GetShrineOffering()
--- More World Locations_AIO/Src/Shrines/ShrineDB.cs
+++ More World Locations_AIO/Src/Shrines/ShrineDB.cs
@@ -15,44 +15,40 @@ public class ShrineDB
     {
         shrinesConfigs = new Dictionary<string, ShrineConfig>
         {
-            // Vanilla Forsaken powers (Extended versions)
-            { "shrineEikthyr",   new ShrineConfig("shrineEikthyr", "Odin's Shrine of Eikthyr",   StatusEffectDB.StatusEffects["MWL_GP_Eikthyr"].StatusEffect,   new Shrine.ShrineOffering("TrophyEikthyr", 1)) },
-            { "shrineElder",     new ShrineConfig("shrineElder", "Odin's Shrine of The Elder", StatusEffectDB.StatusEffects["MWL_GP_TheElder"].StatusEffect,   new Shrine.ShrineOffering("TrophyTheElder", 1)) },
-            { "shrineBonemass",  new ShrineConfig("shrineBonemass", "Odin's Shrine of Bonemass",  StatusEffectDB.StatusEffects["MWL_GP_Bonemass"].StatusEffect,  new Shrine.ShrineOffering("TrophyBonemass", 1)) },
-            { "shrineModer",     new ShrineConfig("shrineModer", "Odin's Shrine of Moder",     StatusEffectDB.StatusEffects["MWL_GP_Moder"].StatusEffect,     new Shrine.ShrineOffering("TrophyDragonQueen", 1)) },
-            { "shrineYagluth",   new ShrineConfig("shrineYagluth", "Odin's Shrine of Yagluth",   StatusEffectDB.StatusEffects["MWL_GP_Yagluth"].StatusEffect,   new Shrine.ShrineOffering("TrophyGoblinKing", 1)) },
-            { "shrineQueen",     new ShrineConfig("shrineQueen", "Odin's Shrine of The Queen", StatusEffectDB.StatusEffects["MWL_GP_Queen"].StatusEffect,     new Shrine.ShrineOffering("TrophySeekerQueen", 1)) },
+            // Meadows
+            { "shrineHealthMeadow", new ShrineConfig("shrineHealthMeadow", "Odin's Shrine of The Boar", StatusEffectDB.StatusEffects["MWL_SE_Boar"].StatusEffect, new Shrine.ShrineOffering("TrophyBoar", 2), Heightmap.Biome.Meadows) },
+            { "shrineStamMeadow", new ShrineConfig("shrineStamMeadow", "Odin's Shrine of The Deer", StatusEffectDB.StatusEffects["MWL_SE_Deer"].StatusEffect, new Shrine.ShrineOffering("TrophyDeer", 2), Heightmap.Biome.Meadows) },
+            { "shrineEitrMeadow", new ShrineConfig("shrineEitrMeadow", "Odin's Shrine of The Neck", StatusEffectDB.StatusEffects["MWL_SE_Neck"].StatusEffect, new Shrine.ShrineOffering("TrophyNeck", 2), Heightmap.Biome.Meadows) },
 
-            // Biome-specific Health Regen
-            { "shrineHealthMeadow", new ShrineConfig("shrineHealthMeadow", "Odin's Shrine of The Boar", StatusEffectDB.StatusEffects["MWL_SE_Meadows_IncreaseHealthRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyBoar", 2)) },
-            { "shrineHealthBlackForest", new ShrineConfig("shrineHealthBlackForest", "Odin's Shrine of The Greydwarf", StatusEffectDB.StatusEffects["MWL_SE_Blackforest_IncreaseHealthRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyGreydwarf", 2)) },
-            { "shrineHealthSwamp", new ShrineConfig("shrineHealthSwamp", "Odin's Shrine of The Draugr", StatusEffectDB.StatusEffects["MWL_SE_Swamp_IncreaseHealthRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyDraugr", 1)) },
-            { "shrineHealthMountain", new ShrineConfig("shrineHealthMountain", "Odin's Shrine of The Wolf", StatusEffectDB.StatusEffects["MWL_SE_Mountains_IncreaseHealthRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyWolf", 1)) },
-            { "shrineHealthPlains", new ShrineConfig("shrineHealthPlains", "Odin's Shrine of The Goblin", StatusEffectDB.StatusEffects["MWL_SE_Plains_IncreaseHealthRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyGoblin", 1)) },
-            { "shrineHealthMistlands", new ShrineConfig("shrineHealthMistlands", "Odin's Shrine of The Seeker", StatusEffectDB.StatusEffects["MWL_SE_Mistlands_IncreaseHealthRegen"].StatusEffect, new Shrine.ShrineOffering("TrophySeeker", 1)) },
+            // Black Forest
+            { "shrineHealthBlackForest", new ShrineConfig("shrineHealthBlackForest", "Odin's Shrine of The Greydwarf", StatusEffectDB.StatusEffects["MWL_SE_Greydwarf"].StatusEffect, new Shrine.ShrineOffering("TrophyGreydwarf", 2), Heightmap.Biome.BlackForest) },
+            { "shrineStamBlackForest", new ShrineConfig("shrineStamBlackForest", "Odin's Shrine of The Skeleton", StatusEffectDB.StatusEffects["MWL_SE_Skeleton"].StatusEffect, new Shrine.ShrineOffering("TrophySkeleton", 2), Heightmap.Biome.BlackForest) },
+            { "shrineEitrBlackForest", new ShrineConfig("shrineEitrBlackForest", "Odin's Shrine of The Greydwarf Shaman", StatusEffectDB.StatusEffects["MWL_SE_GreydwarfShaman"].StatusEffect, new Shrine.ShrineOffering("TrophyGreydwarfShaman", 1), Heightmap.Biome.BlackForest) },
+            { "shrineSkillBlackForest", new ShrineConfig("shrineSkillBlackForest", "Odin's Shrine of The Poison Skeleton", StatusEffectDB.StatusEffects["MWL_SE_SkeletonPoison"].StatusEffect, new Shrine.ShrineOffering("TrophySkeletonPoison", 1), Heightmap.Biome.BlackForest) },
 
-            // Biome-specific Stamina Regen
-            { "shrineStamMeadow", new ShrineConfig("shrineStamMeadow", "Odin's Shrine of The Deer", StatusEffectDB.StatusEffects["MWL_SE_Meadows_IncreaseStamRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyDeer", 2)) },
-            { "shrineStamBlackForest", new ShrineConfig("shrineStamBlackForest", "Odin's Shrine of The Skeleton", StatusEffectDB.StatusEffects["MWL_SE_Blackforest_IncreaseStamRegen"].StatusEffect, new Shrine.ShrineOffering("TrophySkeleton", 2)) },
-            { "shrineStamSwamp", new ShrineConfig("shrineStamSwamp", "Odin's Shrine of The Leech", StatusEffectDB.StatusEffects["MWL_SE_Swamp_IncreaseStamRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyLeech", 1)) },
-            { "shrineStamMountain", new ShrineConfig("shrineStamMountain", "Odin's Shrine of The Hatchling", StatusEffectDB.StatusEffects["MWL_SE_Mountains_IncreaseStamRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyHatchling", 1)) },
-            { "shrineStamPlains", new ShrineConfig("shrineStamPlains", "Odin's Shrine of The Deathsquito", StatusEffectDB.StatusEffects["MWL_SE_Plains_IncreaseStamRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyDeathsquito", 1)) },
-            { "shrineStamMistlands", new ShrineConfig("shrineStamMistlands", "Odin's Shrine of The Hare", StatusEffectDB.StatusEffects["MWL_SE_Mistlands_IncreaseStamRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyHare", 1)) },
+            // Swamp
+            { "shrineHealthSwamp", new ShrineConfig("shrineHealthSwamp", "Odin's Shrine of The Draugr", StatusEffectDB.StatusEffects["MWL_SE_Draugr"].StatusEffect, new Shrine.ShrineOffering("TrophyDraugr", 1), Heightmap.Biome.Swamp) },
+            { "shrineStamSwamp", new ShrineConfig("shrineStamSwamp", "Odin's Shrine of The Leech", StatusEffectDB.StatusEffects["MWL_SE_Leech"].StatusEffect, new Shrine.ShrineOffering("TrophyLeech", 1), Heightmap.Biome.Swamp) },
+            { "shrineEitrSwamp", new ShrineConfig("shrineEitrSwamp", "Odin's Shrine of The Wraith", StatusEffectDB.StatusEffects["MWL_SE_Wraith"].StatusEffect, new Shrine.ShrineOffering("TrophyWraith", 1), Heightmap.Biome.Swamp) },
+            { "shrineSkillSwamp", new ShrineConfig("shrineSkillSwamp", "Odin's Shrine of The Abomination", StatusEffectDB.StatusEffects["MWL_SE_Abomination"].StatusEffect, new Shrine.ShrineOffering("TrophyAbomination", 1), Heightmap.Biome.Swamp) },
 
-            // Biome-specific Eitr Regen
-            { "shrineEitrMeadow", new ShrineConfig("shrineEitrMeadow", "Odin's Shrine of The Neck", StatusEffectDB.StatusEffects["MWL_SE_Meadows_IncreaseEitrRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyNeck", 2)) },
-            { "shrineEitrBlackForest", new ShrineConfig("shrineEitrBlackForest", "Odin's Shrine of The Greydwarf Shaman", StatusEffectDB.StatusEffects["MWL_SE_Blackforest_IncreaseEitrRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyGreydwarfShaman", 1)) },
-            { "shrineEitrSwamp", new ShrineConfig("shrineEitrSwamp", "Odin's Shrine of The Wraith", StatusEffectDB.StatusEffects["MWL_SE_Swamp_IncreaseEitrRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyWraith", 1)) },
-            { "shrineEitrMountain", new ShrineConfig("shrineEitrMountain", "Odin's Shrine of The Ulv", StatusEffectDB.StatusEffects["MWL_SE_Mountains_IncreaseEitrRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyUlv", 1)) },
-            { "shrineEitrPlains", new ShrineConfig("shrineEitrPlains", "Odin's Shrine of The Goblin Shaman", StatusEffectDB.StatusEffects["MWL_SE_Plains_IncreaseEitrRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyGoblinShaman", 1)) },
-            { "shrineEitrMistlands", new ShrineConfig("shrineEitrMistlands", "Odin's Shrine of The Dvergr", StatusEffectDB.StatusEffects["MWL_SE_Mistlands_IncreaseEitrRegen"].StatusEffect, new Shrine.ShrineOffering("TrophyDvergr", 1)) },
+            // Mountains
+            { "shrineHealthMountain", new ShrineConfig("shrineHealthMountain", "Odin's Shrine of The Wolf", StatusEffectDB.StatusEffects["MWL_SE_Wolf"].StatusEffect, new Shrine.ShrineOffering("TrophyWolf", 1), Heightmap.Biome.Mountain) },
+            { "shrineStamMountain", new ShrineConfig("shrineStamMountain", "Odin's Shrine of The Hatchling", StatusEffectDB.StatusEffects["MWL_SE_Hatchling"].StatusEffect, new Shrine.ShrineOffering("TrophyHatchling", 1), Heightmap.Biome.Mountain) },
+            { "shrineEitrMountain", new ShrineConfig("shrineEitrMountain", "Odin's Shrine of The Ulv", StatusEffectDB.StatusEffects["MWL_SE_Ulv"].StatusEffect, new Shrine.ShrineOffering("TrophyUlv", 1), Heightmap.Biome.Mountain) },
+            { "shrineSkillMountain", new ShrineConfig("shrineSkillMountain", "Odin's Shrine of The Cultist", StatusEffectDB.StatusEffects["MWL_SE_Cultist"].StatusEffect, new Shrine.ShrineOffering("TrophyCultist", 1), Heightmap.Biome.Mountain) },
 
-            // Biome-specific Skill Gain
-            { "shrineSkillBlackForest", new ShrineConfig("shrineSkillBlackForest", "Odin's Shrine of The Poison Skeleton", StatusEffectDB.StatusEffects["MWL_SE_Blackforest_IncreaseSkillGain"].StatusEffect, new Shrine.ShrineOffering("TrophySkeletonPoison", 1)) },
-            { "shrineSkillSwamp", new ShrineConfig("shrineSkillSwamp", "Odin's Shrine of The Abomination", StatusEffectDB.StatusEffects["MWL_SE_Swamp_IncreaseSkillGain"].StatusEffect, new Shrine.ShrineOffering("TrophyAbomination", 1)) },
-            { "shrineSkillMountain", new ShrineConfig("shrineSkillMountain", "Odin's Shrine of The Cultist", StatusEffectDB.StatusEffects["MWL_SE_Mountains_IncreaseSkillGain"].StatusEffect, new Shrine.ShrineOffering("TrophyCultist", 1)) },
-            { "shrineSkillPlains", new ShrineConfig("shrineSkillPlains", "Odin's Shrine of The Goblin Brute", StatusEffectDB.StatusEffects["MWL_SE_Plains_IncreaseSkillGain"].StatusEffect, new Shrine.ShrineOffering("TrophyGoblinBrute", 1)) },
-            { "shrineSkillMistlands", new ShrineConfig("shrineSkillMistlands", "Odin's Shrine of The Seeker Brute", StatusEffectDB.StatusEffects["MWL_SE_Mistlands_IncreaseSkillGain"].StatusEffect, new Shrine.ShrineOffering("TrophySeekerBrute", 1)) }
+            // Plains
+            { "shrineHealthPlains", new ShrineConfig("shrineHealthPlains", "Odin's Shrine of The Goblin", StatusEffectDB.StatusEffects["MWL_SE_Goblin"].StatusEffect, new Shrine.ShrineOffering("TrophyGoblin", 1), Heightmap.Biome.Plains) },
+            { "shrineStamPlains", new ShrineConfig("shrineStamPlains", "Odin's Shrine of The Deathsquito", StatusEffectDB.StatusEffects["MWL_SE_Deathsquito"].StatusEffect, new Shrine.ShrineOffering("TrophyDeathsquito", 1), Heightmap.Biome.Plains) },
+            { "shrineEitrPlains", new ShrineConfig("shrineEitrPlains", "Odin's Shrine of The Goblin Shaman", StatusEffectDB.StatusEffects["MWL_SE_GoblinShaman"].StatusEffect, new Shrine.ShrineOffering("TrophyGoblinShaman", 1), Heightmap.Biome.Plains) },
+            { "shrineSkillPlains", new ShrineConfig("shrineSkillPlains", "Odin's Shrine of The Goblin Brute", StatusEffectDB.StatusEffects["MWL_SE_GoblinBrute"].StatusEffect, new Shrine.ShrineOffering("TrophyGoblinBrute", 1), Heightmap.Biome.Plains) },
+
+            // Mistlands
+            { "shrineHealthMistlands", new ShrineConfig("shrineHealthMistlands", "Odin's Shrine of The Seeker", StatusEffectDB.StatusEffects["MWL_SE_Seeker"].StatusEffect, new Shrine.ShrineOffering("TrophySeeker", 1), Heightmap.Biome.Mistlands) },
+            { "shrineStamMistlands", new ShrineConfig("shrineStamMistlands", "Odin's Shrine of The Hare", StatusEffectDB.StatusEffects["MWL_SE_Hare"].StatusEffect, new Shrine.ShrineOffering("TrophyHare", 1), Heightmap.Biome.Mistlands) },
+            { "shrineEitrMistlands", new ShrineConfig("shrineEitrMistlands", "Odin's Shrine of The Dvergr", StatusEffectDB.StatusEffects["MWL_SE_Dvergr"].StatusEffect, new Shrine.ShrineOffering("TrophyDvergr", 1), Heightmap.Biome.Mistlands) },
+            { "shrineSkillMistlands", new ShrineConfig("shrineSkillMistlands", "Odin's Shrine of The Seeker Brute", StatusEffectDB.StatusEffects["MWL_SE_SeekerBrute"].StatusEffect, new Shrine.ShrineOffering("TrophySeekerBrute", 1), Heightmap.Biome.Mistlands) }
         };
         
         ItemManager.OnItemsRegistered -= ShrineDB.BuildShrineConfigs;
@@ -68,6 +64,15 @@ public class ShrineDB
         
         int randomIndex = UnityEngine.Random.Range(0, shrinesConfigs.Count);
         return shrinesConfigs.ElementAt(randomIndex).Value;
+    }
+    
+    public static ShrineConfig GetRandomShrineConfig(Heightmap.Biome biome)
+    {
+        var biomeShrines = shrinesConfigs.Values.Where(s => s.biome == biome).ToList();
+    
+        if (biomeShrines.Count == 0) return null;
+    
+        return biomeShrines[UnityEngine.Random.Range(0, biomeShrines.Count)];
     }
 
     public static ShrineConfig GetShrineConfig(string shrineConfigName)
